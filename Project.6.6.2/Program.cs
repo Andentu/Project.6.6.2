5caec803504789a927ea7ce191200296eb502467
Andentu <severalmaz97@gmail.com>
2023-03-14 20:16:10 +0300

    Добавьте файлы проекта.

---
+++ Project.6.6.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace Project_6_6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    class Pen // Класс
+    {
+        public string color; // Инициализации переменных
+        public int cost;
+
+        public Pen () //Конструктор без параметров
+        {
+            color = "Черный";
+            cost = 100;
+        }
+        public Pen (string PenColor, int PenCost) //С двумя параметрами
+        {
+            color = PenColor;
+            cost = PenCost;
+        }
+    }
+}
